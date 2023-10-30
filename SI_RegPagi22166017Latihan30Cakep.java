386835dd9c66419ecd343ed5c5dff17ef87f5274
silkyafina <148930513+silkyafina@users.noreply.github.com>
2023-10-30 09:10:13 +0700

    Update SI_RegPagi22166017Latihan30Cakep.java

--- SI_RegPagi22166017Latihan30Cakep.java
+++ SI_RegPagi22166017Latihan30Cakep.java
@@ -9,11 +9,16 @@ import java.util.Scanner;
  */
 public class SI_RegPagi22166017Latihan30Cakep {
    public static void main(String[] args) {
+       // Scanner
         Scanner input = new Scanner (System.in);
+       // menampilkan teks berwarna
         System.out.println("\u001b[31mKamu \u001b[32mngerjain sendiri \u001b[33mlatihan 17 sampe \u001b[34mlatihan 30 ini? \u001b[0m");
         System.out.print("\u001b[34mJawab \u001b[31m(Yoi/Enggak) \u001b[0m: ");
+        
+       // menginputkan jawaban
         String jawab = input.next();
         
+      // pengkondisian
         if (jawab.equals("Yoi")) {
             System.out.println("\n\u001b[31mCakep Bener. \u001b[35mGood Job \u001b[0m");
         }else {
